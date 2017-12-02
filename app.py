49ca70583d3eac38900de35ea45a306a6ec54936
Shariq Mustaquim <shariqmus@gmail.com>
2017-12-02 17:05:28 +1100

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def index():
+    return ''
+
+
+@app.route('/post', methods=['POST'])
+def post():
+    print("Data receive =================================================================")
+    print("json {0}".format(request.get_json()))
+    print("Data receive =================================================================")
+
+    if True:
+        return "Yay!", 200
+    else:
+        return "Nay!", 202
+
+if __name__ == '__main__':
+    host = os.getenv('IP', '127.0.0.1')
+    port = int(os.getenv('PORT', 5000))
+    app.debug = True
+    app.run(host=host, port=port)
